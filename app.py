28e24565aaa8180d1f2f003f2332fc6565da31b9
RuijieShen <shenruijie2015@gmail.com>
2025-10-30 00:22:13 -0400

    binding to a port

--- app.py
+++ app.py
@@ -106,9 +106,13 @@ def simulate():
 
  
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
-    app.run(debug=True)
+#    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render provides PORT automatically
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 
